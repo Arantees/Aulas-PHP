012bb563b7f54085fda62069cc35a23c56b4757c
Arantees <lukasarantes59@gmail.com>
2023-10-24 15:46:10 -0300

    att composer

---
+++ composer/buscador-cursos-alura/Functions.php
@@ -0,0 +1,5 @@
+<?php
+
+function Mensagem(string $mensagem){
+    echo $mensagem . PHP_EOL;
+}
--- php-poo/pilha.php
+++ php-poo/pilha.php
@@ -6,7 +6,8 @@ function funcao1()
 
     try {
         funcao2();
-    } catch (Throwable $problema) {
+    } catch (Throwable $problema)
+    {
         echo $problema->getMessage() . PHP_EOL;
         echo $problema->getLine() . PHP_EOL;
         echo $problema->getTraceAsString() . PHP_EOL;
