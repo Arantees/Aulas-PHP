e6c80802a75b7e71f2e2e5d97e26c2972ea96ed7
Arantees <lukasarantes59@gmail.com>
2023-10-26 15:16:50 -0300

    fim-aula-composer

--- composer/buscador-cursos-alura/src/Buscador.php
+++ composer/buscador-cursos-alura/src/Buscador.php
@@ -5,7 +5,6 @@ namespace Alura\BuscadorDeCursos;
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
-
 class Buscador
 {
     private ClientInterface $httpClient;
@@ -14,15 +13,12 @@ class Buscador
 
     public function __construct(ClientInterface $httpClient, Crawler $crawler)
     {
-
         $this->httpClient = $httpClient;
         $this->crawler = $crawler;
-
     }
     public function buscar(string $url): array
     {
         $resposta = $this->httpClient->request('GET', $url);
-
         $html = $resposta->getBody();
 
         $this->crawler->addHtmlContent($html);
@@ -35,5 +31,4 @@ class Buscador
         }
         return $cursos;
     }
-
 }
